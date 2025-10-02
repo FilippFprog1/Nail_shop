63834add5c2ce405f4625d5a59c170378413f932
Filipp <fil.fomin07@inbox.ru>
2025-10-03 00:38:49 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function init() {
+    //вывод товаров на гл. стр.
+    $.getJSON("goods.json", goodsOut);
+}
+
+function goodsOut(data) {
+    //вывод на страницу
+    console.log(data);
+    var out = '';
+    for (var key in data) {
+        out +=`<div class="cart">`;
+        out +=`<p class="name">${data[key].name}</p>`;
+        out +=`<img src="images/${data[key].img}" alt=""></img>`;
+        out +=`<div class="cost">${data[key].cost}</div>`;
+        out +=`<button class="add-to-cart">Купить</button>`;
+        out +=`</div>`;
+    }
+    $('.goods-out').html(out);
+}
+
+$(document).ready(function () {
+    init();
+});
