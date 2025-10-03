f4a3677d5f94c8dcead57a40124c03c04ea2b00e
Filipp <fil.fomin07@inbox.ru>
2025-10-03 13:08:40 +0300

    4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var cart = {};
+
 function init() {
     //вывод товаров на гл. стр.
     $.getJSON("goods.json", goodsOut);
@@ -5,7 +7,6 @@ function init() {
 
 function goodsOut(data) {
     //вывод на страницу
-    console.log(data);
     var out = '';
     for (var key in data) {
         out +=`<div class="cart">`;
@@ -13,10 +14,44 @@ function goodsOut(data) {
         out +=`<img src="images/${data[key].img}" alt=""></img>`;
         out +=`<p class="descript">${data[key].description}</p>`;
         out +=`<div class="cost">${data[key].cost}</div>`;
-        out +=`<button class="add-to-cart">Купить</button>`;
+        out +=`<button class="add-to-cart" data-id="${key}">Купить</button>`;
         out +=`</div>`;
     }
     $('.goods-out').html(out);
+    $('.add-to-cart').on('click', addToCart);
+}
+
+function addToCart() {
+    var id = $(this).attr('data-id');
+    console.log(id);
+    if (cart[id] == undefined) {
+        cart[id] = 1;
+    }
+    else {
+        cart[id]++;
+    }
+    showMiniCart();
+    saveCart();
+}
+
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function showMiniCart() {
+    //показываем мини корзину
+    var out="";
+    for (var key in cart) {
+        out += key +' --- '+ cart[key]+'<br>';
+    }
+    $('.mini-cart').html(out);
+}
+
+function loadCart() {
+    if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+        showMiniCart();
+    }
 }
 
 $(document).ready(function () {
