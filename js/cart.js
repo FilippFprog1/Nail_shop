8ce58bf7fce4e44b6e933d39ba6ad932239368cd
Filipp <fil.fomin07@inbox.ru>
2025-10-03 13:37:42 +0300

    5

---
+++ js/cart.js
@@ -0,0 +1,25 @@
+var cart = {};
+function loadCart() {
+    if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+        showCart();
+    }
+    else {
+        $('.main-cart').html('Корзина пуста');
+    }
+}
+
+function showCart() {
+    $.getJSON('goods.json', function (data) {
+        var goods = data;
+        var out = '';
+        for (var key in cart) {
+            out += `<img src="images\${goods[id].img}">`;
+        }
+        $('.main-cart').html(out);
+    });
+}
+
+$(document).ready(function () {
+    loadCart();
+});
