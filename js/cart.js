4ee74d8385e6d1c32e4488e10601e1f91bb51bc1
Filipp <fil.fomin07@inbox.ru>
2025-10-03 17:22:26 +0300

    5

--- js/cart.js
+++ js/cart.js
@@ -14,12 +14,27 @@ function showCart() {
         var goods = data;
         var out = '';
         for (var id in cart) {
+            out += `<button data-id=${id} class="del-goods">-</button>`
             out += `<img src="images/${goods[id].img}">`;
-            out +=`<p class="descript">${goods[id].description}</p>`;
+            out += `${goods[id].name}`;
+            out += `${cart[id]}`;
             out +=`<div class="cost">${goods[id].cost}</div>`;
+            out +=`<br>`;
         }
         $('.main-cart').html(out);
+        $('.del-goods').on('click', delGoods);
     });
+}
+
+function delGoods() {
+    var id = $(this).attr('data-id');
+    delete cart[id];
+    saveCart();
+    showCart();
+}
+
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 $(document).ready(function () {
