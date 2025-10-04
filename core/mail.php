bfdabee073d8af6d6178e3a2e05c49aa869cc29d
Filipp <fil.fomin07@inbox.ru>
2025-10-05 00:27:12 +0300

    main commit

--- core/mail.php
+++ core/mail.php
@@ -9,3 +9,12 @@ $message .= '<h1>Заказ в магазине</h1>';
 $message .= '<p>Телефон: '.$_POST['ephone'].'</p>';
 $message .= '<p>Почта: '.$_POST['email'].'</p>';
 $message .= '<p>Клиент: '.$_POST['ename'].'</p>';
+
+$cart = $_POST['cart'];
+
+foreach ($cart as $id=>$count) {
+    $message .=$json[$id]['name'];
+}
+print_r($message);
+
+
